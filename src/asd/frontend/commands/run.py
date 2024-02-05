3113326f235c78b82bf09c970df507eed87d34af
Alejandro Navas <caeus@Alejandros-MacBook-Pro.local>
2024-02-05 18:41:04 -0500

    ok

--- src/asd/frontend/commands/run.py
+++ src/asd/frontend/commands/run.py
@@ -3,15 +3,22 @@ from dataclasses import dataclass
 import click
 
 from asd.frontend.dispatcher import CmdDispatcher
-from asd.kernel import RunCmd, TaskQuery
+from asd.kernel import RunCmd, TaskQuery, WorkspaceAt
 
 @dataclass(frozen=True)
 class RunCmdHandler:
     impl:Final[click.Command]
-def create_run_command(dispatcher:CmdDispatcher)->RunCmdHandler:
+def create_run_command(dispatcher:CmdDispatcher,workspace_at:WorkspaceAt)->RunCmdHandler:
     @RunCmdHandler
     @click.command()
     @click.argument('query')
     def run(query:str)->None:
+        print(workspace_at)
+        print(workspace_at)
+        print(workspace_at)
+        print(workspace_at)
+        print(workspace_at)
+        print(workspace_at)
+        print(workspace_at)
         dispatcher(RunCmd(TaskQuery.from_str(query)))
     return run
