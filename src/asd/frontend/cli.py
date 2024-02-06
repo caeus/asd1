ca762a170eebd17c35b73eefbf664d996ffd5420
Alejandro Navas <caeus@Alejandros-MacBook-Pro.local>
2024-02-05 20:27:52 -0500

    ok

--- src/asd/frontend/cli.py
+++ src/asd/frontend/cli.py
@@ -1,21 +1,19 @@
 
 
-
-
-
 import click
 from asd.frontend.commands.env import EnvCmdHandler
+from asd.frontend.commands.query import QueryCmdHandler
 from asd.frontend.commands.run import RunCmdHandler
 
 
 def create_cli(
-        run:RunCmdHandler,
-        launcher:EnvCmdHandler
-)->click.Group:
+        run: RunCmdHandler,
+        env: EnvCmdHandler,
+        query: QueryCmdHandler
+) -> click.Group:
     @click.group()
-    def cli()->None: ...
+    def cli() -> None: ...
     cli.add_command(run.impl)
-    cli.add_command(launcher.impl)
+    cli.add_command(env.impl)
+    cli.add_command(query.impl)
     return cli
-    
-    
